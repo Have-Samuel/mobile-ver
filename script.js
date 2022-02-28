b0b93ef1596869de0a192b984e0267b9980d06ad
Have Samuel <samiehave9@gmail.com>
2022-02-28 21:20:37 +0300

    Modify HTML, CSS & JS files

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.querySelector('ham-menu');
+
+const navmenu = document.querySelector('.navmenu');
+
+menuIcon.addEventListener('click', () => {
+  navmenu.classList.toggle('change');
+});
