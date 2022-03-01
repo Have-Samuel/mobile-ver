90f75d5a31edf1a01adcee560bd9b4b16eee283e
Have Samuel <samiehave9@gmail.com>
2022-03-01 18:44:25 +0300

    Making the page dynamic using JS file

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ listItems.forEach((element) => {
     menuLinks.classList.remove('change');
   });
 });
+
+const allProjects = document.querySelector('.project');
+const popup = document.querySelector('.popup-project');
+
+const projects = [
+  {
+    projectId: 'projectOne',
+    name: 'Printing Data One',
+    description: 'A daily selection of privately personalized reads; no accounts or '
+    + 'sign-ups required. has been the industry\'s standard',
+    featuredImage: 'images/Background.png',
+    technologies: ['html', 'Bootstrap', 'Ruby'],
+    liveLink: 'https://have-samuel.github.io/portfolio1/'
+    sourceLink: 'https://github.com/Have-Samuel/portfolio1/tree/popup-menu',
+  }
+];
+
+const arrClassName = ['project1', 'project2', 'project3', 'project4', 'project5', 'project6'];
+
