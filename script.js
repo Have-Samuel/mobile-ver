53c7506717df930694a0debe639ecda75d4f87ba
Have Samuel <samiehave9@gmail.com>
2022-03-02 02:47:32 +0300

    Writing the cards dynamically

--- script.js
+++ script.js
@@ -72,21 +72,12 @@ function createProjectCard(project) {
     sectionCards.appendChild(project);
   }
 
-const projectDetails = {
-  name: 'Multi Post Stories',
-  techs: ['html', 'Bootstrap', 'Ruby on rails'],
-  imgSrc: './images/#',
-  description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-  liveSrc: '#',
-  sourceLink: '#'
-}
-
 function window (){
 const blurDiv = document.createElement('div')
 blurDiv.classList.add('blur')
   
   let title = document.createElement('h3');
-  title.innerHTML = projectDetails.name;
+  title.innerHTML = projects.name;
   blurDiv.appendChild(title);
   
   
